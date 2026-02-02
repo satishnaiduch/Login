93686588ecd3bfea6d87d7029c1e981c65cee3d5
satishnaiduch <satishsatyam603@gmail.com>
2026-02-02 21:48:59 +0530

    code added 1

--- src/test/java/com/test/login/LoginTest.java
+++ src/test/java/com/test/login/LoginTest.java
@@ -3,27 +3,27 @@ package com.test.login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
 	 @Test
 	    public void loginTest() {
-		 ChromeOptions options = new ChromeOptions();
-		 options.addArguments("--headless");
-		 options.addArguments("--no-sandbox");
-		 options.addArguments("--disable-dev-shm-usage");
+	        // Headless mode for CI/CD
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
 
-		 WebDriver driver = new ChromeDriver(options);
-	        //WebDriver driver = new ChromeDriver();
+	        WebDriver driver = new ChromeDriver(options);
 
-	        driver.get("https://the-internet.herokuapp.com/login");
+	        driver.get("https://example.com/login");
 
-	        driver.findElement(By.id("username")).sendKeys("tomsmith");
-	        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-	        driver.findElement(By.cssSelector("button[type='submit']")).click();
+	        driver.findElement(By.id("username")).sendKeys("testuser");
+	        driver.findElement(By.id("password")).sendKeys("test123");
+	        driver.findElement(By.id("loginBtn")).click();
 
 	        driver.quit();
 	    }
-
 }
